7a727334a03def2fde0c993f60098309b3e39f82
hemanthcse1 <hemanthcse1@gmail.com>
2024-05-31 19:48:49 +0530

    Types of Thread pools 1. Fixed Thread pool 2. CachedThreadPool 3. ScheduledThreadPool 4. SingleThreadPool

--- src/main/java/basics/TestThread.java
+++ src/main/java/basics/TestThread.java
@@ -2,6 +2,8 @@ package basics;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TestThread {
 
@@ -20,7 +22,7 @@ public class TestThread {
 
 
         // using executor
-        ExecutorService service = Executors.newFixedThreadPool(5);
+      /*  ExecutorService service = Executors.newFixedThreadPool(5);
 
         for (int i = 0; i< 5; i++){
             service.execute(new Runnable() {
@@ -29,7 +31,71 @@ public class TestThread {
                     System.out.println("Running thread using executor ");
                 }
             });
+        }*/
+
+
+        System.out.println("\n-------------------------------\n");
+
+
+        for (int i = 0; i< 10; i++){
+            Thread testThread = new Thread(new Task());
+            testThread.start();
         }
+
+
+        System.out.println("\n-------------------------------\n");
+
+        // execute 100 threads using Executor with fixed thread pool size 10
+        /*ExecutorService service1 = Executors.newFixedThreadPool(10);
+        for (int i = 0; i< 100; i++){
+            service1.execute(new Task());
+        }*/
+
+        System.out.println("\n-------------------------------\n");
+
+        // Get number of available processors in the system
+        int coresCount = Runtime.getRuntime().availableProcessors();
+        System.out.println("Number of cores "+coresCount);
+
+        ExecutorService service1 = Executors.newFixedThreadPool(coresCount);
+
+        System.out.println("\n-------------------------------\n");
+
+        // Types of Thread pools
+        // 1. Fixed size thread pool
+        ExecutorService fixedThreadService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i< 10; i++){
+            fixedThreadService.execute(new Task());
+        }
+
+
+        System.out.println("\n-------------------------------\n");
+
+        // 2. cachedThreadPool
+        ExecutorService cachedThreadService = Executors.newCachedThreadPool();
+        cachedThreadService.execute(new Task());
+
+
+        System.out.println("\n-------------------------------\n");
+
+
+        // 3. Scheduled Thread pool
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(10);
+        scheduledExecutorService.schedule(new Task(), 10, TimeUnit.SECONDS);
+
+        scheduledExecutorService.scheduleAtFixedRate(new Task(),10, 15,TimeUnit.SECONDS);
+
+        scheduledExecutorService.scheduleWithFixedDelay(new Task(),15,15,TimeUnit.SECONDS);
+
+
+
+        System.out.println("\n-------------------------------\n");
+
+        // 4. Single Thread pool
+        ExecutorService singleThreadService = Executors.newSingleThreadExecutor();
+
+        singleThreadService.execute(new Task());
 
     }
 }
